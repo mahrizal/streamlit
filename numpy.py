d152f9bc1a62bb4972c3053b3b41a46383024c90
Mahrizal <mahrizal_nu@yahoo.co.id>
2023-08-31 22:35:38 +0700

    balikin lagi

--- numpy.py
+++ numpy.py
@@ -8,13 +8,3 @@ st.title("Nama Karyawan MUC IT")
 
 df = pd.DataFrame({"nama": ["Mahrizal", "Naf"], "email" : ["mahrizal_nu@yahoo.co.id", "naf@yahoo.co.id"]})
 df
-
-
-tickerSymbol = 'GOOGL'
-
-tickerData = yf.Ticker(tickerSymbol)
-
-tickerDf  = tickerData.history(period='1d', start='2010-5-31', end='2020-5-31')
-
-st.line_chart(tickerDf.Close)
-st.line_chart(tickerDf.volume)
