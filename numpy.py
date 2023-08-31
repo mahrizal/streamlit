ecade6ead388034e16d064de59bc06872c1b8bd5
Mahrizal <mahrizal_nu@yahoo.co.id>
2023-08-31 21:44:01 +0700

    Menambahkan title

--- numpy.py
+++ numpy.py
@@ -2,5 +2,7 @@ import numpy as np
 import pandas as pd
 import streamlit as st
 
+st.title("Nama Karyawan MUC")
+
 df = pd.DataFrame({"nama": ["Mahrizal", "Naf"], "email" : ["mahrizal_nu@yahoo.co.id", "naf@yahoo.co.id"]})
 df
