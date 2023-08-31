359eb3a7fd2e889ae0987a96851f63754fb035d4
Mahrizal <mahrizal_nu@yahoo.co.id>
2023-08-31 21:45:12 +0700

    Tambah kata IT

--- numpy.py
+++ numpy.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import streamlit as st
 
-st.title("Nama Karyawan MUC")
+st.title("Nama Karyawan MUC IT")
 
 df = pd.DataFrame({"nama": ["Mahrizal", "Naf"], "email" : ["mahrizal_nu@yahoo.co.id", "naf@yahoo.co.id"]})
 df
