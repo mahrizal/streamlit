bd2bf10d7f5c632096b97961f39fd3dd7d32a2b2
Mahrizal <mahrizal_nu@yahoo.co.id>
2023-08-31 21:49:17 +0700

    menambahkan matplotlib

--- numpy.py
+++ numpy.py
@@ -6,3 +6,11 @@ st.title("Nama Karyawan MUC IT")
 
 df = pd.DataFrame({"nama": ["Mahrizal", "Naf"], "email" : ["mahrizal_nu@yahoo.co.id", "naf@yahoo.co.id"]})
 df
+
+import matplotlib.pyplot as plt
+
+arr = np.random.normal(1, 1, size=100)
+fig, ax = plt.subplots()
+ax.hist(arr, bins=20)
+
+fig  # 👈 Draw a Matplotlib chart
